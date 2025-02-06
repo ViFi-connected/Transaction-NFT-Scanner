b18d4acddbb04f381a1e9568fe2f56a61e98be68
Filip Viduna <viduna.f@gmail.com>
2025-02-06 11:40:04 +0100

    HashSet

--- TransactionProcessor.cs
+++ TransactionProcessor.cs
@@ -7,11 +7,17 @@ namespace TransactionNftScanner
         public static async Task IterateDataAndDownloadImages(TransactionUTXOs transactionUTXOs, string dirPath)
         {
             List<TransactionUTXOs.InputOutput> IOs = new(transactionUTXOs.inputs.Concat(transactionUTXOs.outputs));
+            HashSet<string> processedAssets = new();
 
             foreach (var IO in IOs)
             {
                 foreach (var asset in IO.amount.Where(x => x.unit != "lovelace" && x.quantity == "1"))
                 {
+                    if (processedAssets.Contains(asset.unit))
+                    {
+                        continue;
+                    }
+
                     var assetApiPath = $"assets/{asset.unit}";
                     var specificAsset = await HttpClientHelper.GetData<SpecificAsset>(assetApiPath).ConfigureAwait(false);
                     if (specificAsset == null) { continue; }
@@ -20,6 +26,9 @@ namespace TransactionNftScanner
                     var path = $"{dirPath}\\{name}.png";
 
                     await HttpClientHelper.DownloadImage(ipfsHash, path).ConfigureAwait(false);
+                    Console.WriteLine($"Successfully downloaded image for asset: {name} to path: {path}\n");
+
+                    processedAssets.Add(asset.unit);
                 }
             }
         }
